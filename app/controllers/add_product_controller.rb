96466b80122bb4e6221fa5b1d41d3746c26b719a
JiradaWi <cutiemoon08@yahoo.com>
2018-03-30 15:06:49 +0700

    Add category

--- app/controllers/add_product_controller.rb
+++ app/controllers/add_product_controller.rb
@@ -15,4 +15,10 @@ class AddProductController < ApplicationController
     @sql = 'INSERT INTO product (Name, Description, Price, CatId, Image) VALUES(\''+params['Name']+'\',\''+params['Description']+'\',\''+params['Price']+'\','+params['catId']+',\''+params['Image']+'\')'
     ActiveRecord::Base.connection.execute(@sql)
   end
+
+  def addCat
+    @data = params['catname']
+    ActiveRecord::Base.connection.execute('INSERT INTO category (Name) VALUES (\''+@data+'\')')
+    redirect_to('/welcome/index')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'welcome/add_product', to: 'add_product#index'
   get 'add_product/create', to: 'add_product#create'
+  get 'add_product/addCat', to: 'add_product#addCat'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
